1042a7ea7a8af67a0e318c20945a8d47d57f14be
mayank812sharma <mayank.sharma.mitus@gmail.com>
2026-04-08 11:23:49 +0530

    no_of_occurence

---
+++ no of occurence/no_of_occurence.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+int first_occurence(int arr[], int n, int key)
+{
+
+  int start = 0;
+  int end = n - 1;
+  int mid = start + (end - start) / 2;
+  int ans = -1;
+  while (start <= end)
+  {
+    if (arr[mid] == key)
+    {
+      ans = mid;
+      end = mid - 1;
+    }
+    else if (arr[mid] < key)
+    {
+      start = mid + 1;
+    }
+    else
+    {
+      end = mid - 1;
+    }
+    mid = start + (end - start) / 2;
+  }
+  return ans+1;
+}
+
+int last_occurence(int arr[], int n, int key)
+{
+
+  int start = 0;
+  int end = n - 1;
+  int mid = start + (end - start) / 2;
+  int ans = -1;
+  while (start <= end)
+  {
+    if (arr[mid] == key)
+    {
+      ans = mid;
+      start = mid + 1;
+    }
+    else if (arr[mid] < key)
+    {
+      start = mid + 1;
+    }
+    else
+    {
+      end = mid - 1;
+    }
+    mid = start + (end - start) / 2;
+  }
+  return ans+1;
+}
+
+int main()
+{
+  int n;
+  cout << "Enter The length of the array -> ";
+  cin >> n;
+
+  int arr[1000];
+  cout << "Enter the array -> ";
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+
+  int key;
+  cout << "Enter the key which you want to search -> ";
+  cin >> key;
+
+  // cout << "The first occurrence is at -> " << first_occurence(arr, n, key) << endl;
+  // cout << "The last occurrence is at -> " << last_occurence(arr, n, key) << endl;
+
+  cout<<"The number of occurence is -> "<<(last_occurence(arr,n,key)-first_occurence(arr,n,key))+1<<endl;
+  return 0;
+}
